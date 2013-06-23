bf14e6ac104447bdce7a7235b2c7087b76d439b4
waldron <waldron@5750G.(none)>
2013-06-23 12:51:28 +0800

    uno queen see this, im chinese

--- help/classRel.cpp
+++ help/classRel.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
+
 class Animal{
     public:
-        Animal(){}
+        int age; // attribute often puts here.
+        Animal(){
+            this->age = 5; // I give age a value here.
+        }
         ~Animal(){}
-        int age;
-}
+};
+
 class Dog:public Animal{
     public:
         Dog(){}
         ~Dog(){}
-        int SetDog{
-            cout<<"dog is"<<age;
+        int SetDog (){ //where is the defination of age?
             cin>>age;
+            cout<<"dog is "<<age<<" ."<<endl;
+            // if u want to change age and see it change, log it after your alternation.
         }
-}
-int main(){
+};
+int main()
+{
     Dog dog;
-    Dog.SetDog();
-    cout<<"dog is from animal":
-        return 0;
+    for (;;) {
+        dog.SetDog();// dog, not Dog. Dog is a class, dog is an instant.
+        cout<<"dog is from animal"<<endl;
+        cout <<"Press Y to continue."<<endl;// If u want to verify your change
+        char c;//you should put it in a loop and log them out
+        cin >> c;
+        if ((c != 'y')&&(c  != 'Y')) //
+            break; // these statements can be deleted.
+    }
+    return 0;
 } 
